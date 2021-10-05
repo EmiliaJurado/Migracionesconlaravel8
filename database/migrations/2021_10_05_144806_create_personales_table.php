0a420377631f8e9566245d3b3bb5641be3093e11
EmiliaJurado <emily_9814@hotmail.com>
2021-10-05 10:16:34 -0500

    version1

---
+++ database/migrations/2021_10_05_144806_create_personales_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personales', function (Blueprint $table) {
+            $table->id();
+            $table->string('apellido_paterno');
+            $table->string('apellido_materno');
+            $table->string('nombre');
+            $table->date('fecha_de_nacimiento');
+            $table->string('sexo');
+            $table->string('correo')->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personales');
+    }
+}
